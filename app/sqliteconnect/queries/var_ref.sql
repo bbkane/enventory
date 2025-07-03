06b17eeea668a9f607904de3a12e1bd5d243789c
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-07-03 08:05:05 -0400

    gen db update with sqlc

--- app/sqliteconnect/queries/var_ref.sql
+++ app/sqliteconnect/queries/var_ref.sql
@@ -23,3 +23,13 @@ SELECT env.name AS env_name, var_ref.* FROM var_ref
 JOIN env ON var_ref.env_id = env.env_id
 WHERE var_id = ?
 ORDER BY var_ref.name ASC;
+
+-- name: VarRefUpdate :execrows
+UPDATE var_ref SET
+    env_id = COALESCE(sqlc.narg('env_id'), env_id),
+    name = COALESCE(sqlc.narg('name'), name),
+    comment = COALESCE(sqlc.narg('comment'), comment),
+    create_time = COALESCE(sqlc.narg('create_time'), create_time),
+    update_time = COALESCE(sqlc.narg('update_time'), update_time),
+    var_id = COALESCE(sqlc.narg('var_id'), var_id)
+WHERE var_ref_id = sqlc.arg('var_ref_id');
