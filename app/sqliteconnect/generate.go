2cf7cb3ead3361972aa8b8e0822e6b2016aa55b5
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-07-22 21:31:10 -0700

    Download latest version of sqlc during sqlc gen

--- app/sqliteconnect/generate.go
+++ app/sqliteconnect/generate.go
@@ -1,4 +1,5 @@
 package sqliteconnect
 
+//go:generate go get -tool -modfile=go.tool.mod github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 //go:generate go tool -modfile=go.tool.mod sqlc vet
 //go:generate go tool -modfile=go.tool.mod sqlc generate
