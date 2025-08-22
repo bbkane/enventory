ab8f95a85d2fe64057ac83176f10856db328caee
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-08-22 15:12:38 -0700

    Start env_ref table

---
+++ app/sqliteconnect/migrations/007_env_ref.sql
@@ -0,0 +1,18 @@
+-- create env_ref table to reference envs from other envs
+
+CREATE TABLE env_ref (
+    env_ref_id INTEGER PRIMARY KEY,
+    env_id INTEGER NOT NULL, -- env that contains us
+    ref_env_id INTEGER NOT NULL, -- env that we reference
+    comment TEXT NOT NULL,
+    create_time TIMESTAMP NOT NULL,
+    update_time TIMESTAMP NOT NULL,
+    FOREIGN KEY(env_id) REFERENCES env(env_id) ON DELETE CASCADE,
+    FOREIGN KEY(ref_env_id) REFERENCES env(env_id) ON DELETE RESTRICT,
+    UNIQUE(env_id, ref_env_id)
+);
+
+CREATE INDEX ix_env_ref_env_id ON env_ref(env_id);
+CREATE INDEX ix_env_ref_ref_env_id ON env_ref(ref_env_id);
+
+-- next let's create our env_export view, then add the triggers, then delete the old view and triggers
