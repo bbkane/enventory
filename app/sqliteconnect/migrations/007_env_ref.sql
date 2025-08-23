07ea6fd987cb1865ef0adce0cc2a19fa9a09a0cd
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-08-23 09:58:38 -0700

    env ref table tweaks

--- app/sqliteconnect/migrations/007_env_ref.sql
+++ app/sqliteconnect/migrations/007_env_ref.sql
@@ -5,12 +5,12 @@ CREATE TABLE env_ref (
     env_id INTEGER NOT NULL, -- env that contains us
     ref_env_id INTEGER NOT NULL, -- env that we reference
     comment TEXT NOT NULL,
-    create_time TIMESTAMP NOT NULL,
-    update_time TIMESTAMP NOT NULL,
+    create_time TEXT NOT NULL,
+    update_time TEXT NOT NULL,
     FOREIGN KEY(env_id) REFERENCES env(env_id) ON DELETE CASCADE,
     FOREIGN KEY(ref_env_id) REFERENCES env(env_id) ON DELETE RESTRICT,
     UNIQUE(env_id, ref_env_id)
-);
+) STRICT;
 
 CREATE INDEX ix_env_ref_env_id ON env_ref(env_id);
 CREATE INDEX ix_env_ref_ref_env_id ON env_ref(ref_env_id);
